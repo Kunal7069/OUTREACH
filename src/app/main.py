0bcfdf7a7032af13c6b2422cdcc452f2ea85e3b2
Kunal7069 <jaink7069@gmail.com>
2025-06-20 21:39:05 +0530

    cookies added

--- src/app/main.py
+++ src/app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .config.database.database import Base, engine
 from src.app.routers import user,document
-
+import requests
 # Create all tables
 Base.metadata.create_all(bind=engine)
 
@@ -24,3 +24,42 @@ app.add_middleware(
 # Register routes
 app.include_router(user.router)
 app.include_router(document.router)
+
+@app.get("/login-cookies")
+def login_and_get_cookies():
+    url = "https://api.prod.kiwiq.ai/api/v1/auth/login?keep_me_logged_in=true"
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    payload = {
+        "grant_type": "password",
+        "username": "raunak@kiwiq.ai",
+        "password": "L3gP0yWZ8dK6jH4g"
+    }
+
+    try:
+        response = requests.post(url, data=payload, headers=headers)
+
+        if response.status_code == 200:
+            cookies = response.cookies
+            return  {
+                    "refresh_token": cookies.get("refresh_token"),
+                    "access_token": cookies.get("access_token"),
+                    "XSRF-TOKEN": cookies.get("XSRF-TOKEN")
+                }
+            
+        else:
+            return {
+                "status": "failed",
+                "status_code": response.status_code,
+                "message": response.text
+            }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+        
