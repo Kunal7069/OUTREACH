f2054e7ed89533043d3d23410622a07179ff7b21
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2025-06-30 19:24:11 +0530

    Update database.py

--- src/app/config/database/database.py
+++ src/app/config/database/database.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL,pool_pre_ping=True,    
+    pool_recycle=60)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
